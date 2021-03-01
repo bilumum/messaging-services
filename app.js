b18f11f7890fa6974f699e2897d607ac06cf3f76
bilumum <umit.altuntas@hotmail.com>
2021-03-01 23:03:22 +0300

    socket implemantation

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express");
-var cors = require('cors')
+var cors = require('cors');
+//var io = require('socket.io');
 
-const onlineUsersRoute = require('./routes/onlineUsers')
+//var onlineUsers = [];
+const onlineUsersRoute = require('./routes/onlineUsers').router;
+var logoutUser = require('./routes/onlineUsers').logoutUser ;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 app.use("/users", onlineUsersRoute);
 
@@ -14,6 +16,41 @@ app.use("/", function(req, res){
     return res.send("node api is working...");
 });
 
-app.listen(8080, () => {
+var server = app.listen(8080, () => {
     console.log("Server running on port 8080");
+});
+
+//io.listen(server);
+var io = require('socket.io')(server,{
+  cors: {
+    origin: '*',
+  }
+});
+
+io.sockets.on('connection', function (socket) {
+  
+  let connectedUser = socket.handshake.auth;
+
+  //console.log(socket.handshake);
+
+  if(connectedUser){
+  
+    console.log(connectedUser.name + ' ' + connectedUser.surname + " connected");
+
+    socket.broadcast.emit("NEW_JOIN", connectedUser.name + ' ' + connectedUser.surname + "geldi");
+   
+    socket.on('disconnect', () => {
+
+      //onlineUsers = onlineUsers.filter(user => user.userId === connectedUser.userId);
+      logoutUser(connectedUser);
+      //console.log(onlineUsers);
+
+      socket.broadcast.emit("NEW_JOIN", connectedUser.name + ' ' + connectedUser.surname + "gitti");
+      console.log(connectedUser.name + ' ' + connectedUser.surname + " disconnected");
+    });
+
+  }
+
+  
+
 });
--- routes/onlineUsers.js
+++ routes/onlineUsers.js
@@ -1,7 +1,10 @@
 var express = require("express");
 
+const crypto = require("crypto");
+
 const router = express.Router();
-const onlineUsers = [];
+
+var onlineUsers = [];
 
 router.get("/", (req, res, next) => {
 
@@ -10,11 +13,25 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+    req.body.userId = crypto.randomBytes(16).toString("hex");
+   
+    var foundIndex = onlineUsers.findIndex(user => user.name.trim() === req.body.name.trim() && user.surname.trim() === req.body.surname.trim());
 
-    console.log(req);
+    if(foundIndex > -1){
+        res.status(500).send({
+            error: "This user has already joined chat. Please use another name&surname :)"
+        });
+        //res.status(500, { error: "This user has already joined chat. Please use another name&surname :)" });
+    }
+
     onlineUsers.push(req.body);
     res.status(201).json(req.body);
 
 });
 
-module.exports = router;
+function logoutUser(loggedOutUser){
+   console.log(loggedOutUser);
+    onlineUsers = onlineUsers.filter(user => user.userId !== loggedOutUser.userId);
+}
+
+module.exports = {router, logoutUser};
